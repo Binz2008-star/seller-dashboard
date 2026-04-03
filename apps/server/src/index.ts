073c68c468ee991532bb58a0aaa18637958bd8d1
Robin Edwan.1 <robenedwan.1@icloud.com>
2026-04-03 16:55:53 +0400

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,39 @@
+import { trpcServer } from "@hono/trpc-server";
+import { createContext } from "@seller-dashboard/api/context";
+import { appRouter } from "@seller-dashboard/api/routers/index";
+import { auth } from "@seller-dashboard/auth";
+import { env } from "@seller-dashboard/env/server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use(logger());
+app.use(
+  "/*",
+  cors({
+    origin: env.CORS_ORIGIN,
+    allowMethods: ["GET", "POST", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
+
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+    createContext: (_opts, context) => {
+      return createContext({ context });
+    },
+  }),
+);
+
+app.get("/", (c) => {
+  return c.text("OK");
+});
+
+export default app;
